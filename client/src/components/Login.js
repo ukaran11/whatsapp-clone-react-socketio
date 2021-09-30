055b04ff243421a3b5dfca8d77518b7ca54059c1
ukaran11 <ukaran11@gmail.com>
2021-10-01 02:53:28 +0530

    Utsav | Added Login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,17 @@
-import React from 'react'
-import { Container } from 'react-bootstrap';
+import React, {useRef} from 'react'
+import { Container, Form } from 'react-bootstrap';
+
 export default function Login() {
+    const idRef = useRef();
+
     return (
-        <Container>
-            Hello Login
+        <Container className="align-items-center d-flex" style={{height:'100vh'}}>
+            <Form className="w-100">
+                <Form.Group>
+                    <Form.Label>Enter Your ID</Form.Label>
+                    <Form.Control type="text" ref={idRef} required/>
+                </Form.Group>
+            </Form>
         </Container>
     )
 }
