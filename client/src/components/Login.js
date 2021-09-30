1849743762590d06b8d8284fa16056edbb04cfbf
ukaran11 <ukaran11@gmail.com>
2021-10-01 02:47:07 +0530

    Utsav | Basic skeleton

---
+++ client/src/components/Login.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Container } from 'react-bootstrap';
+export default function Login() {
+    return (
+        <Container>
+            Hello Login
+        </Container>
+    )
+}
