06220007645dc7cf510017a54af4ac3ae186d51a
ukaran11 <ukaran11@gmail.com>
2021-10-03 01:24:51 +0530

    Utsav | Implemented Contacts

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { ContactsProvider } from '../context/ContactsProvider';
 import { useLocalStorage } from '../hooks/useLocalStorage';
 import Dashboard from './Dashboard';
 import Login from "./Login";
 
 const App = () => {
   const [id, setId] = useLocalStorage('id');
+
+  const dashboard = (
+    <ContactsProvider>
+      <Dashboard id={id} />
+    </ContactsProvider>
+  )
   return (
-    id ? <Dashboard id={id} /> : <Login onIdSubmit={setId} />
+    id ? dashboard : <Login onIdSubmit={setId} />
   );
 }
 
